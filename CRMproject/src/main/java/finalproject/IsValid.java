8a316916825ca846f4fa25e1f997ae32119656cd
MyQuynh <59412967+MyQuynh@users.noreply.github.com>
2021-01-03 22:30:43 +0700

    Update IsValid.java
    Adding the isExistLeadID method

--- CRMproject/src/main/java/finalproject/IsValid.java
+++ CRMproject/src/main/java/finalproject/IsValid.java
@@ -1,5 +1,11 @@
 package finalproject;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class IsValid {
@@ -168,6 +174,59 @@ public class IsValid {
         }
 
         return bufferUpperCase.toString();
+
+    }
+
+    public String isExistLeadID(){
+
+        // Creating the array to store exist LeadID
+        ArrayList<String> listLead = new ArrayList<>();
+
+        try {
+            FileReader fileWriter = new FileReader(new File("leads.csv"));
+            Scanner scannerFile = new Scanner(fileWriter);
+
+
+            // Looping through the file and stop till the end of the file
+            while (scannerFile.hasNext()){
+
+                // Split the line into word array and get the leadID store into the listLead array
+                String[] word = scannerFile.nextLine().split(",");
+                listLead.add(word[0]);
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Cannot read the file");;
+        }
+
+        // Looping until the users enter the valid LeadID
+        while(true){
+
+            // Get the input from users
+            System.out.print("Enter interaction's lead's id: ");
+            String leadID = Main.scanner.next();
+
+            // If the input is empty, make the user enter again
+            if (leadID.equals("")){
+                System.out.println("The LeadID cannot be empty");
+                System.out.println("Please try again");
+            } else {
+
+                // Looping through the listLead to see if it match th input from users
+                for (int i = 0; i < listLead.size(); i++) {
+
+                    // If it match, return the leadID
+                    if(leadID.equals(listLead.get(i))){
+                        return leadID;
+                    }
+                }
+                // If does not, make the user enter again
+                System.out.println("The lead ID is not in the system\nPlease try again");
+
+            }
+
+
+        }
 
     }
 
